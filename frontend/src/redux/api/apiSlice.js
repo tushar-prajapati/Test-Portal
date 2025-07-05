029ca128b46678395472a6dc7e22396ed31174fe
Tushar Prajapati <tusharprt5@gmail.com>
2025-07-06 00:39:57 +0530

    bug fixes

--- frontend/src/redux/api/apiSlice.js
+++ frontend/src/redux/api/apiSlice.js
@@ -2,6 +2,7 @@ import {fetchBaseQuery, createApi} from '@reduxjs/toolkit/query/react'
 import { BASE_URL } from '../constants.js'
 
 const baseQuery = fetchBaseQuery({baseUrl: BASE_URL,
+    credentials: 'include',
 
 })
 
