b0dcbceef55bcc0fea525abb0ebfb9b77a1a633a
Tushar Prajapati <tusharprt5@gmail.com>
2025-07-06 00:24:24 +0530

    bug fixes

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -8,8 +8,8 @@ import jwt  from "jsonwebtoken";
 
     res.cookie('jwt', token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV !=='development',
-        sameSite:'strict',
+        secure: true,
+        sameSite:'none',
         maxAge: 30*24*68*68*1000
     })
     return token;
