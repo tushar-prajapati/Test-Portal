7717e38ad58add99c9533789e56b4f2024ae0573
Tushar Prajapati <tusharprt5@gmail.com>
2025-07-06 00:28:14 +0530

    bug fixes

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ connectDB();
 const app = express();
 
 app.use(cors({
-    origin: "https://test-portal-bay.vercel.app/",
+    origin: "https://test-portal-bay.vercel.app",
   credentials: true,
 }))
 app.use(express.json());
